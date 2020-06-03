9a46aff0e7bab310f30dd06a05881ac8bb45a57e
uhyeon19 <uhyeon1009@gmail.com>
2020-06-03 20:24:40 +0900

    commit 2

--- hello.c
+++ hello.c
@@ -2,5 +2,12 @@
 
 int main(){
     printf("Hello World!\n");
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
     return 0;
 }
